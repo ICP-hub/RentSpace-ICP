ab1cd7903e36c32757f49146cc68d504fdb19275
abhishekpandit5599 <abhishekpandit5599@gmail.com>
2023-12-02 11:19:11 +0530

    configer

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 /**
  * @format
  */
-import "react-native-polyfill-globals/auto";
-import "react-native-fetch-api";
-import "fast-text-encoding";
+import 'react-native-polyfill-globals/auto';
+import 'react-native-fetch-api';
+import 'fast-text-encoding';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
 
-AppRegistry.registerComponent(appName, () => App);
+const Stack = createNativeStackNavigator();
+
+const linking = {
+  prefixes: ['rentspace://'],
+};
+
+const RootComponent: React.FC = () => {
+  return (
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator initialRouteName="Launch">
+        <Stack.Screen name="Launch" component={App} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => RootComponent);
