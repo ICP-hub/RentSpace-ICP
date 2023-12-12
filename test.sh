27d53af43205e8e6769e1d3bd375c86ab24b5fbb
Mohd-anas0693 <mohdanas0.693@gmail.com>
2023-12-12 19:24:06 +0530

    updated test.sh

--- test.sh
+++ test.sh
@@ -10,12 +10,17 @@ hotelCanister=$(dfx canister call backend createNewHotelCanister hotelCanister |
 echo "---------Accessing user Canister and creating account of the user, using principal of idenitity in terminal----------"
 dfx canister call $userCanister createUser '("Rahul","Sharma","18-sep-2003","rahul@gmail.com","user")'
 echo "---------Accessing user Canister and executing getPK function and returns canister name, using principal of idenitity in terminal----------"
-dfx canister call $userCanister getPK
+dfx canister call $userCanister getUserInfo
 echo "---------Accessing user Canister and executing updateUserInfo and returns canister name, using principal of idenitity in terminal----------"
 dfx canister call $userCanister updateUserInfo '(record {firstName="Anas"; lastName="chung";dob="12/09/2004";userEmail = "anas@gmail.com";userType="user";userProfile="random"; userGovId= "sddffdf"; hostStatus= false; verificationStatus= true;})'
 echo "---------Accessing user Canister and  getOwner the idenitity  of owner in terminal----------"
 dfx canister call $userCanister getOwner
 echo "---------Accessing Hotel canister and  register the hotel with hotel id of user  in terminal----------"
-userUuid=$(dfx canister call $hotelCanister createHotel '(record {hotelTitle= "venture";hotelDes="Huge rooms";hotelImage="djnjvnjvnfd";hotelPrice="$2000";hotelLocation="Ludhiana"})'| grep -oP 'rzo6e-othar-as4dz-5dm3l-5mlxo-q7w3x-5ol3f-74pvr-s2mbw-vdigk-yae#[0-9a-f\-]+')
+dfx canister call $hotelCanister createHotel '(record {hotelTitle= "venture";hotelDes="Huge rooms";hotelImage="djnjvnjvnfd";hotelPrice="$2000";hotelLocation="Ludhiana"})'
 echo "---------Accessing hotel Canister and inserting Hotel Id HotelIds linked with userId, using principal of idenitity in terminal----------"
-dfx canister call $hotelCanister getHotelId $userUuid
+hotelUuid=$(dfx canister call $hotelCanister getHotelId | grep -o '"[^"]*"' | awk 'NR==1')
+echo $hotelUuid
+echo "---------Accessing hotel Canister and inserting Hotel Id HotelIds linked with userId, using principal of idenitity in terminal----------"
+dfx canister call $hotelCanister getHotel "(""$hotelUuid"")"
+echo "---------Accessing hotel Canister and updateinng  Hotel Id HotelIds , using principal of idenitity in terminal----------"
+dfx canister call $hotelCanister updateHotel '(record {hotelTitle= "Indane";hotelDes="Comfy rooms";hotelImage="djnjdseed";hotelPrice="$120000";hotelLocation="London"})'
