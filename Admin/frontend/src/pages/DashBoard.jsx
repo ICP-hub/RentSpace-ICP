b75e64bf00fa3b19a71ba4d2e625931f16af5e45
Mohd-anas0693 <mohdanas0.693@gmail.com>
2024-02-27 23:04:52 +0530

    added code in the useEffect

--- Admin/frontend/src/pages/DashBoard.jsx
+++ Admin/frontend/src/pages/DashBoard.jsx
@@ -1,32 +1,52 @@
-import React, { useState } from 'react'
-import {useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from '../components/Reusables/header/Header'
 import '../components/dashboard/dashboard.css'
 import InfoCards from '../components/dashboard/InfoCards'
 import ChatWindow from '../components/dashboard/chatWindow/ChatWindow'
 import BookingChart from '../components/dashboard/bookingChart/BookingChart'
 import Navbar from '../components/Reusables/menuNavBar/Navbar'
+import { useAuth } from '../utils/useAuthClient';
+
 
 const DashBoard = () => {
-  const nav=useNavigate()
-  const [chat,showChat]=useState(true)
+  const nav = useNavigate();
+  const [bookingData, setBookingData] = useState(null);
+  const [hotelOnBoard, setHotelOnBoardData] = useState(null);
+  const [userOnBoardData, setUserOnBoardData] = useState(null);
+  const [chat, showChat] = useState(true);
+  const { actors } = useAuth();
+
+  async function updateAnnualData() {
+    let year = new Date().getFullYear();
+    console.log(year + " type of year: " + typeof (year));
+    setBookingData(await actors.bookingActor.getBookingFrequencyInYear(year.toLocaleString()));
+    console.log(" BookingData: " + bookingData);
+    setHotelOnBoardData(await actors.hotelActor.getHotelFrequencyByYear(year.toLocaleString()));
+
+  };
+
+  console.log(actors);
+  useEffect(() => {
+    getBookingFrequencyInYear();
+  }, []);
   return (
     <div className='page'>
-      <Navbar nav={nav}/>
-      <Header title={'Dashboard Overview'} onClick={()=>showChat(!chat)}/>
+      <Navbar nav={nav} />
+      <Header title={'Dashboard Overview'} onClick={() => showChat(!chat)} />
       <div className='dashboard-main'>
         <div className='dashboard-main-info'>
-            <div className='dashboard-infoCards-cont'>
-              <InfoCards label={"Users"} count={45}/>
-              <InfoCards label={"Hotels"} count={345}/>
-            </div>
-            <BookingChart/>
+          <div className='dashboard-infoCards-cont'>
+            <InfoCards label={"Users"} count={45} />
+            <InfoCards label={"Hotels"} count={345} />
+          </div>
+          <BookingChart />
         </div>
         {
-          chat?
-          <ChatWindow nav={nav}/>
-          :
-          <></>
+          chat ?
+            <ChatWindow nav={nav} />
+            :
+            <></>
         }
       </div>
     </div>
