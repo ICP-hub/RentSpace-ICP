3ed22c1054919741bdc4f050b83be4be6b04adf0
rajnishQuadb <rajnish.tripathi@quadbtech.com>
2024-04-17 16:32:07 +0530

    hide Login button after login

--- LoginWeb/src/index.js
+++ LoginWeb/src/index.js
@@ -1,6 +1,6 @@
 import {backend, createActor} from '../../src/declarations/backend/index';
 import {AuthClient} from '@dfinity/auth-client';
-import {HttpAgent,fromHex} from '@dfinity/agent';
+import {HttpAgent, fromHex} from '@dfinity/agent';
 import {
   DelegationIdentity,
   Ed25519PublicKey,
@@ -10,12 +10,12 @@ import {
 
 let actor = backend;
 var url = new URL(window.location.href);
-let authClient
-// One day in nanoseconds  
-const days = BigInt(1);  
-const hours = BigInt(24);  
+let authClient;
+// One day in nanoseconds
+const days = BigInt(1);
+const hours = BigInt(24);
 const nanoseconds = BigInt(3600000000000);
-const numDays=BigInt(5)// number of days delegation is valid for
+const numDays = BigInt(5); // number of days delegation is valid for
 // Get the search parameters from the URL
 var params = new URLSearchParams(url.search);
 
@@ -23,7 +23,7 @@ const loginButton = document.getElementById('login');
 loginButton.onclick = async e => {
   e.preventDefault();
 
-//   var middleKeyIdentity = await ECDSAKeyIdentity.generate({extractable: true});
+  //   var middleKeyIdentity = await ECDSAKeyIdentity.generate({extractable: true});
   let publicKey = params.get('publicKey');
   let newIdentity = new ECDSAKeyIdentity(
     {publicKey: fromHex(publicKey)},
@@ -33,11 +33,12 @@ loginButton.onclick = async e => {
   authClient = await AuthClient.create({identity: newIdentity});
   await new Promise(resolve => {
     authClient.login({
-      identityProvider: process.env.DFX_NETWORK === "ic"
-                  ? "https://identity.ic0.app"
-                  : `http://localhost:4943/?canisterId=rdmx6-jaaaa-aaaaa-aaadq-cai`,
+      identityProvider:
+        process.env.DFX_NETWORK === 'ic'
+          ? 'https://identity.ic0.app'
+          : `http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943`,
 
-      maxTimeToLive: days * hours * nanoseconds * numDays, 
+      maxTimeToLive: days * hours * nanoseconds * numDays,
       onSuccess: resolve,
     });
   });
@@ -48,13 +49,33 @@ loginButton.onclick = async e => {
 
   const encodedDelegation = encodeURIComponent(delegationString);
 
-  var url = `rentspace://auth?delegation=${encodedDelegation}`;
-  window.open(url, '_self');
+  loginButton.style.display = 'none';
+
+  console.log(encodedDelegation);
+
+  // const resp = await ECDSAKeyIdentity.generate
+
+  // var url = `rentspace://auth?delegation=${encodedDelegation}`;
+  // window.open(url, '_self');
+  // const chain = DelegationChain.fromJSON(
+  //   JSON.parse(decodeURIComponent(encodedDelegation)),
+  // );
+  // const middleIdentity = DelegationIdentity.fromDelegation(
+  //   newIdentity,
+  //   chain,
+  // );
+  // const agent = new HttpAgent({identity: middleIdentity});
+  // const actor = createActor("be2us-64aaa-aaaaa-qaabq-cai",{agent});
+  // const result = await actor.whoami()
+  // console.log(result)
+
+  console.log(identity.getPrincipal().toText());
 
   return false;
 };
-const redirectBtn=document.getElementById('open')
-redirectBtn.onclick=()=>{
+
+const redirectBtn = document.getElementById('open');
+redirectBtn.onclick = () => {
   const identity = authClient.getIdentity();
 
   var delegationString = JSON.stringify(identity.getDelegation().toJSON());
@@ -63,5 +84,5 @@ redirectBtn.onclick=()=>{
 
   var url = `rentspace://auth?delegation=${encodedDelegation}`;
   window.open(url, '_self');
-  return false
-}
+  return false;
+};
