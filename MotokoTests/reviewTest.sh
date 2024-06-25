b1c184800744b0c70fde49b89b2d6cb864c82a4e
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-06-25 18:01:26 +0530

    Review Test

--- MotokoTests/reviewTest.sh
+++ MotokoTests/reviewTest.sh
@@ -1,26 +1,73 @@
-echo "stopping canister"
-echo "---------Stoping dfx----------"
-dfx stop
-echo "---------Starting dfx----------"
-dfx start --clean --background
-echo "---------Deploying Backend Canister----------"
-dfx deploy backend
-userCanister=$(dfx canister call backend createNewUserCanister userCanister | grep -oP '(?<=\")(.*?)(?=\")')
-hotelCanister=$(dfx canister call backend createNewHotelCanister hotelCanister | grep -oP '(?<=\")(.*?)(?=\")')
-echo "---------Accessing user Canister and creating account of the user, using principal of idenitity in terminal----------"
-dfx canister call $userCanister createUser '(record {firstName="Mohammad "; lastName="Anas";dob="18/09/2003";userEmail = "mohdanaspctebca@0@gmail.com";})'
-echo "---------Accessing user Canister and executing getPK function and returns canister name, using principal of idenitity in terminal----------"
-dfx canister call $userCanister getUserInfo
-echo "---------Accessing user Canister and executing updateUserInfo and returns canister name, using principal of idenitity in terminal----------"
-dfx canister call $userCanister updateUserInfo '(record {firstName="Mohammad "; lastName="Anas";dob="18/09/2003";userEmail = "mohdanaspctebca@0@gmail.com";userType="user";userProfile="random"; userGovId= "sddffdf"; hostStatus= false; verificationStatus= true;})'
-echo "---------Accessing user Canister and  getOwner the idenitity  of owner in terminal----------"
-dfx canister call $userCanister getOwner
-echo "---------Accessing Hotel canister and  register the hotel with hotel id of user  in terminal----------"
-dfx canister call $hotelCanister createHotel '(record {hotelTitle= "venture";hotelDes="Huge rooms";hotelImage="djnjvnjvnfd";hotelPrice="$2000";hotelLocation="Ludhiana"})'
-echo "---------Accessing hotel Canister and inserting Hotel Id HotelIds linked with userId, using principal of idenitity in terminal----------"
-hotelUuid=$(dfx canister call $hotelCanister getHotelId | grep -o '"[^"]*"' | awk 'NR==1')
-echo $hotelUuid
-echo "---------Accessing hotel Canister and inserting Hotel Id HotelIds linked with userId, using principal of idenitity in terminal----------"
-dfx canister call $hotelCanister getHotel "(""$hotelUuid"")"
-echo "---------Accessing hotel Canister and updateinng  Hotel Id HotelIds , using principal of idenitity in terminal----------"
-dfx canister call $hotelCanister updateHotel "("$hotelUuid",record {hotelTitle= 'Indane';hotelDes='Comfyrooms';hotelImage='djnjdseed';hotelPrice='120000';hotelLocation='London'})"
+userCanister="User"
+hotelCanister="Hotel"
+reviewCanister="Review"
+
+
+# user variables
+name="user1"
+email="u@gmail.com"
+date="12/11/2012"
+samplestr="sdksd,xl,x"
+principal=$(dfx identity get-principal)
+
+# hotel variables
+hname="hotel1"
+des="sdnjndwndwdnimwidnwindWdiwndiwndiwd"
+price=300
+location="haryana"
+image="img1"
+hdate="12/11/2012"
+hotelId=""
+
+# Room variables
+roomType="Single"
+roomPrice=300
+echo "$hotelId"
+
+
+echo "---------------Creating new user for testing---------------"
+dfx canister call $userCanister registerUser '(record {
+         firstName="'${name}'"; 
+         lastName="'${name}'";
+         dob="'${date}'";
+         userEmail = "'${email}'";
+     })'
+
+echo "------------------Creating new hotel for testing---------------------"
+hotelID=$(dfx canister call $hotelCanister createHotel '(record {
+         hotelId= "'${hotelId}'";
+         hotelTitle= "'${hname}'";
+         hotelDes="'${des}'";
+         hotelImage="'${image}'";
+         hotelLocation="'${location}'";
+         hotelAvailableFrom="'${hdate}'";
+         hotelAvailableTill="'${hdate}'";
+         createdAt="'${hdate}'"
+    },
+    record {
+        roomType="'${roomType}'";
+        roomPrice='${roomPrice}';
+    }
+    )'| grep -o '"[^"]*"' | awk 'NR==1')
+
+echo "'${hotelID}'"
+
+echo "-------------------Creating new Review-------------------"
+
+dfx canister call $reviewCanister createReview '('${hotelID}', record {
+    hotelId='${hotelID}';
+    rating=4.0;
+    title="This is a review";
+    des="This is a review description";
+})'
+
+echo "-------------------Getting all Reviews-------------------"
+
+dfx canister call $reviewCanister getAllReviewsOnHotel '('${hotelID}')'
+
+echo "--------------Getting Hotel Rating---------------"
+dfx canister call $reviewCanister getHotelRating '('${hotelID}')'
+
+echo "--------------Calling Who Am I---------------"
+
+dfx canister call $reviewCanister whoami
