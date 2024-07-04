571722b0413833bc62ec5e1f85da6dda009d4ded
atharav-bhatnagar-qb <atharav.bhatnagar@quadbtech.com>
2024-07-04 15:46:10 +0530

    admin panel declarations updated, new integration added in dashboard

--- Admin/AdminDevelopmentConfig.js
+++ Admin/AdminDevelopmentConfig.js
@@ -4,35 +4,21 @@ const production=false
 export const host=(production)?"https://icp-api.io":"http://127.0.0.1:4943"
 
 export const ids={
-    userCan:(production)?
-        // "tassb-3qaaa-aaaan-qloxq-cai":
-        "wenzb-uyaaa-aaaan-qlwsa-cai":
-        "dmalx-m4aaa-aaaaa-qaanq-cai",
-        
-    hotelCan:(production)?
-        "wkpuj-piaaa-aaaan-qlwta-cai":
-        // "5mldq-waaaa-aaaan-qlqta-cai":
-        "dxfxs-weaaa-aaaaa-qaapa-cai",
-        
-    backendCan:(production)?
-        // "thtuv-wiaaa-aaaan-qloxa-cai":
-        "5cjoy-nqaaa-aaaan-qlqsa-cai":
-        "be2us-64aaa-aaaaa-qaabq-cai",
-    reviewCan:(production)?
-        "wdm7v-zaaaa-aaaan-qlwsq-cai":
-        "cuj6u-c4aaa-aaaaa-qaajq-cai",
-    bookingCan:(production)?
-        "xhbqh-aaaaa-aaaan-qlwuq-cai":
-        "dzh22-nuaaa-aaaaa-qaaoa-cai",
-    tokenCan:(production)?
-        "ryjl3-tyaaa-aaaaa-aaaba-cai"
-        :"bw4dl-smaaa-aaaaa-qaacq-cai",
-    commentCan:(production)?
-        "wnos5-cqaaa-aaaan-qlwtq-cai"
-        :
-        "dfdal-2uaaa-aaaaa-qaama-cai",
-    supportCan:(production)?
-        "xaawt-nyaaa-aaaan-qlwua-cai"
-        :
-        "dqerg-34aaa-aaaaa-qaapq-cai"
+    userCan: production
+    ? 'ttotv-cyaaa-aaaao-a3o2a-cai'
+    // : 'by6od-j4aaa-aaaaa-qaadq-cai', // rajnish
+    :"avqkn-guaaa-aaaaa-qaaea-cai", // atharva
+
+  hotelCan: production
+    ? 'tilpq-yaaaa-aaaao-a3oyq-cai'
+    // : 'be2us-64aaa-aaaaa-qaabq-cai', // rajnish
+    :"br5f7-7uaaa-aaaaa-qaaca-cai", // atharva
+  bookingCan: production
+    ? 'rsxhm-gqaaa-aaaao-a3oxq-cai'
+    // : 'bkyz2-fmaaa-aaaaa-qaaaq-cai', // rajnish
+    :"bd3sg-teaaa-aaaaa-qaaba-cai", // atharva
+  supportCan: production
+    ? 'tgjcy-dqaaa-aaaao-a3ozq-cai'
+    // : 'b77ix-eeaaa-aaaaa-qaada-cai', // rajnish
+    :"by6od-j4aaa-aaaaa-qaadq-cai", // atharva
 }
